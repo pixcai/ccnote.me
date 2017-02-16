90e5f77cef6614bbda5c00bc9e1776bbd464b69f
pixcai <pixcai@sierra.local>
2017-02-17 02:57:08 +0800

    Add docker config

---
+++ ccnote.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# -*- coding: utf-8 -*-
+
+if [ $# -gt 0 ] && [ $1 == stop ]; then
+	docker stop php7.1-fpm nginx >> /dev/null
+	docker rm php7.1-fpm nginx
+else
+	docker run --name php7.1-fpm -d hub.c.163.com/library/php:7.1-fpm
+	docker run --name nginx -d -p 8080:8080 --link php7.1-fpm -v $(pwd):/var/www -v `pwd`/ccnote.conf:/etc/nginx/conf.d/ccnote.conf:ro --volumes-from php7.1-fpm hub.c.163.com/library/nginx:latest
+fi
+
---
+++ install-images.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# -*- coding: utf-8 -*-
+
+docker pull hub.c.163.com/library/nginx:latest
+docker pull hub.c.163.com/library/php:7.1-fpm
+
